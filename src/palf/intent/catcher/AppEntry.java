ff7fddd73f648e8f4bc8dfaa14bf8b46d1141824
palf <jack.palf@gmail.com>
2013-03-03 11:33:55 +0000

    dded internet permission to app

--- src/palf/intent/catcher/AppEntry.java
+++ src/palf/intent/catcher/AppEntry.java
@@ -5,7 +5,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import com.github.kevinsawicki.http.HttpRequest;
+
 import java.util.Set;
+
 
 public class AppEntry extends Activity {
 
@@ -20,17 +23,23 @@ public class AppEntry extends Activity {
 			Set<String> keys = bundle.keySet();
 			for (String key : keys) {
 				String value = bundle.get(key).toString();
-				String pair = key + " : " + value;
-				message += pair + "\n";
+				if(key == "playlisturl") {
+					message = value;
+				}
 			}
 		} catch (Exception e) {
-			message = "no data sent with intent (probably)";
+			message = "no data sent with intent (or an exception occurred!)";
 		}
+
+    String xbmcURL = "http://192.168.1.74:3000/control?action=play&pid=b01r5mbc";
+    // int response = HttpRequest.get(xbmcURL).code();
+    // String code = Integer.toString(response);
 
 		TextView textView = new TextView(this);
 		textView.setText(message);
 
 		setContentView(textView);
 	}
+
 
 }
